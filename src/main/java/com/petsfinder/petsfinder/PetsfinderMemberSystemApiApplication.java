0b0d2eb856dd8e0b713b837e63097ed131464110
jwhouse <gnsl0127@naver.com>
2023-03-29 13:42:46 +0900

    Initial Commit!

---
+++ src/main/java/com/petsfinder/petsfinder/PetsfinderMemberSystemApiApplication.java
@@ -0,0 +1,13 @@
+package com.petsfinder.petsfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetsfinderMemberSystemApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetsfinderMemberSystemApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/petsfinder/petsfinder/PetsfinderMemberSystemApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petsfinder.petsfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetsfinderMemberSystemApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
